1cfe539b830ccce81552f20aa2593227b9592694
Aguante River pa <162610600+MicaelGimenez@users.noreply.github.com>
2024-05-27 13:41:29 -0300

    Add files via upload

---
+++ estructurasecuencial3.py
@@ -0,0 +1,17 @@
+partidos_jugados = 0
+partidos_ganados = 0
+partidos_empatados = 0
+partidos_perdidos = 0
+puntaje_total= 0
+
+partidos_jugados = int(input("ingrese la cantidad de partidos jugados"))
+partidos_ganados = int(input("ingrese la cantidad de partidos ganados"))
+partidos_empatados = int(input("ingrese la cantidad de partidos empatados"))
+partidos_perdidos = int(input("ingrese la cantidad de partidos perdidos"))
+
+if partidos_jugados != (partidos_ganados + partidos_empatados + partidos_perdidos):
+    print("Error! La suma de partidos ganados, empatados y perdidos no coincide con el total de partidos jugados")
+    exit()
+
+puntaje_total = (partidos_ganados + 3) + partidos_empatados
+print(f"El equipo lleva (puntaje_total) puntos acumulados en el campeonato.")
